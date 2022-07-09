e9891ffc990c511bf809e73a514ceb64983acf0c
Cameron MacFarland <distantcam@gmail.com>
2022-07-09 10:39:43 +0800

    More testing and better code gen

--- src/AutoCtor.Tests/SourceGenerationTests.cs
+++ src/AutoCtor.Tests/SourceGenerationTests.cs
@@ -10,18 +10,15 @@ public class SourceGenerationTests
     [InlineData("[AutoConstructAttribute]")]
     [InlineData("[AutoCtor.AutoConstruct]")]
     [InlineData("[AutoCtor.AutoConstructAttribute]")]
-    public Task GeneratesWithAllAttributeOptions(string attribute)
+    public Task AttributeTest(string attribute)
     {
-        var syntaxTree = CSharpSyntaxTree.ParseText(@$"
-{attribute}public class GeneratesWithAllAttributeOptions {{}}
-");
+        var code = @$"
+{attribute}public partial class TestClass {{}}
+";
+        var compilation = Compile(code);
 
-        var compilation = CSharpCompilation.Create("AutoCtorTest", new[] { syntaxTree });
         var generator = new AutoConstructSourceGenerator();
-
-        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
-
-        driver = driver.RunGenerators(compilation);
+        var driver = CSharpGeneratorDriver.Create(generator).RunGenerators(compilation);
 
         return Verify(driver).UseParameters(attribute);
     }
@@ -34,22 +31,72 @@ public class SourceGenerationTests
     [InlineData("IEnumerable<string>")]
     [InlineData("List<int>")]
     [InlineData("int[]")]
-    public Task GeneratesWithDifferentTypes(string type)
+    [InlineData("int?")]
+    [InlineData("Nullable<int>")]
+    public Task TypesTest(string type)
     {
-        var syntaxTree = CSharpSyntaxTree.ParseText(@$"
+        var code = @$"
 using System;
 using System.Collections.Generic;
 
-[AutoConstruct]public class GeneratesWithDifferentTypes {{ private readonly {type} _item; }}
-");
+[AutoConstruct]public partial class TestClass {{ private readonly {type} _item; }}
+";
+        var compilation = Compile(code);
 
-        var compilation = CSharpCompilation.Create("AutoCtorTest", new[] { syntaxTree });
         var generator = new AutoConstructSourceGenerator();
-
-        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
-
-        driver = driver.RunGenerators(compilation);
+        var driver = CSharpGeneratorDriver.Create(generator).RunGenerators(compilation);
 
         return Verify(driver).UseParameters(type);
+    }
+
+    [Fact]
+    public Task FriendlyParameterNamesTest()
+    {
+        var code = @"
+[AutoConstruct]public partial class TestClass
+{
+    private readonly int _underscorePrefix;
+    private readonly int camelCase;
+    private readonly int PascalCase;
+}";
+        var compilation = Compile(code);
+
+        var generator = new AutoConstructSourceGenerator();
+        var driver = CSharpGeneratorDriver.Create(generator).RunGenerators(compilation);
+
+        return Verify(driver);
+    }
+
+    [Fact]
+    public Task NamespaceTest()
+    {
+        var code = @"
+namespace TestNamespace
+{
+    [AutoConstruct]public partial class TestClass
+    {
+        private readonly int _item;
+    }
+}";
+        var compilation = Compile(code);
+
+        var generator = new AutoConstructSourceGenerator();
+        var driver = CSharpGeneratorDriver.Create(generator).RunGenerators(compilation);
+
+        return Verify(driver);
+    }
+
+    private static CSharpCompilation Compile(params string[] code)
+    {
+        var references = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(assembly => !assembly.IsDynamic)
+            .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
+            .Cast<MetadataReference>();
+
+        return CSharpCompilation.Create(
+            "AutoCtorTest",
+            code.Select(c => CSharpSyntaxTree.ParseText(c)),
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
     }
 }
---
+++ src/AutoCtor/CodeBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace AutoCtor;
+
+public class CodeBuilder
+{
+    private readonly StringBuilder _stringBuilder = new();
+    private int _indent = 0;
+
+    public void IncreaseIndent() => _indent++;
+    public void DecreaseIndent() => _indent--;
+    public void AppendLine(string line) => _stringBuilder.AppendLine(new string('\t', _indent) + line);
+    public override string ToString() => _stringBuilder.ToString();
+}
