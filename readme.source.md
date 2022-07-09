872ada5b90a7979595ac9fb6acd986b9bcdeb110
Cameron MacFarland <distantcam@gmail.com>
2022-07-09 11:24:40 +0800

    Added github bits

---
+++ readme.source.md
@@ -0,0 +1,19 @@
+# AutoCtor
+
+[![NuGet Status](https://img.shields.io/nuget/v/AutoCtor.svg?label=AutoCtor)](https://www.nuget.org/packages/AutoCtor/)
+
+AutoCtor is a Roslyn Source Generator that will automatically create a constructor for your class for use with constructor Dependency Injection.
+
+## NuGet packages
+
+https://nuget.org/packages/AutoCtor/
+
+## Usage
+
+### Your code
+
+snippet: YourCode
+
+### What gets generated
+
+snippet: GeneratedCode
---
+++ src/AutoCtor.Example/ExampleClass.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoCtor.Example;
+
+public interface ICustomService { }
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class AutoConstruct : Attribute { }
+
+#region YourCode
+
+[AutoConstruct]
+public partial class ExampleClass
+{
+    private readonly ICustomService _customService;
+}
+
+#endregion
+
+#region GeneratedCode
+
+partial class ExampleClass
+{
+    public ExampleClass(ICustomService customService)
+    {
+        _customService = customService;
+    }
+}
+
+#endregion
