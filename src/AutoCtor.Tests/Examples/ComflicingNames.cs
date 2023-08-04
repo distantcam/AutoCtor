e05c4de61c99785e021dfce7f69331275bd262ca
Cam MacFarland <distantcam@gmail.com>
2023-08-04 21:32:28 +0800

    Fixing issue with generated parameters using the same name #85

---
+++ src/AutoCtor.Tests/Examples/ComflicingNames.cs
@@ -0,0 +1,19 @@
+﻿using AutoCtor;
+
+[AutoConstruct]
+public partial class AClass
+{
+    private readonly IExampleA _example;
+}
+
+[AutoConstruct]
+public partial class BClass : AClass
+{
+    private readonly IExampleB _example;
+}
+
+[AutoConstruct]
+public partial class CClass : BClass
+{
+    private readonly IExampleC _example;
+}
--- src/AutoCtor/AutoConstructSourceGenerator.cs
+++ src/AutoCtor/AutoConstructSourceGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections;
+using System.Collections.Immutable;
 using AutoSource;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -22,10 +23,9 @@ public class AutoConstructSourceGenerator : IIncrementalGenerator
 
     private static void GenerateSource(SourceProductionContext context, ImmutableArray<ITypeSymbol?> types)
     {
-        if (types.IsDefaultOrEmpty)
-            return;
+        if (types.IsDefaultOrEmpty) return;
 
-        var ctorMaps = new Dictionary<ITypeSymbol, IEnumerable<Parameter>>(SymbolEqualityComparer.Default);
+        var ctorMaps = new Dictionary<ITypeSymbol, ParameterList>(SymbolEqualityComparer.Default);
         var orderedTypes = types.OfType<INamedTypeSymbol>().OrderBy(t =>
         {
             var count = 0;
@@ -50,25 +50,30 @@ public class AutoConstructSourceGenerator : IIncrementalGenerator
                 {
                     if (ctorMaps.TryGetValue(type.BaseType.ConstructUnboundGenericType(), out var temp))
                     {
-                        baseParameters = temp.ToArray();
+                        var baseParameterList = new List<Parameter>();
                         var typedArgs = type.BaseType.TypeArguments;
                         var typedParameters = type.BaseType.TypeParameters;
-                        foreach (var bp in baseParameters)
+                        foreach (var bp in temp)
                         {
+                            var bpName = bp.Name;
+                            var bpType = bp.Type;
                             for (var i = 0; i < typedParameters.Length; i++)
                             {
                                 if (SymbolEqualityComparer.Default.Equals(typedParameters[i], bp.Type))
                                 {
-                                    bp.Type = typedArgs[i];
+                                    bpType = typedArgs[i];
                                     break;
                                 }
                             }
+                            baseParameterList.Add(new Parameter(bpType, bpName));
                         }
+                        baseParameters = baseParameterList;
                     }
                 }
                 else
                 {
-                    ctorMaps.TryGetValue(type.BaseType, out baseParameters);
+                    ctorMaps.TryGetValue(type.BaseType, out var temp);
+                    baseParameters = temp?.ToList();
                 }
             }
 
@@ -87,12 +92,7 @@ public class AutoConstructSourceGenerator : IIncrementalGenerator
         }
     }
 
-    private static bool HasFieldInitialiser(IFieldSymbol symbol)
-    {
-        return symbol.DeclaringSyntaxReferences.Select(x => x.GetSyntax()).OfType<VariableDeclaratorSyntax>().Any(x => x.Initializer != null);
-    }
-
-    private static (SourceText, IEnumerable<Parameter>) GenerateSource(ITypeSymbol type, IEnumerable<Parameter>? baseParameters = default)
+    private static (SourceText, ParameterList) GenerateSource(ITypeSymbol type, IEnumerable<Parameter>? baseParameters = default)
     {
         var ns = type.ContainingNamespace.IsGlobalNamespace
                 ? null
@@ -102,27 +102,22 @@ public class AutoConstructSourceGenerator : IIncrementalGenerator
             .OfType<IFieldSymbol>()
             .Where(f => f.IsReadOnly && !f.IsStatic && f.CanBeReferencedByName && !HasFieldInitialiser(f));
 
-        var parameters = fields.Select(CreateParameter);
-
-        var baseCtorParameters = Enumerable.Empty<Parameter>();
-        var baseCtorArgs = Enumerable.Empty<string>();
+        var parameters = new ParameterList(fields);
 
         if (type.BaseType != null)
         {
             var constructor = type.BaseType.Constructors.OnlyOrDefault(c => !c.IsStatic && c.Parameters.Any());
             if (constructor != null)
             {
-                baseCtorParameters = constructor.Parameters.Select(CreateParameter);
-                baseCtorArgs = constructor.Parameters.Select(p => CreateFriendlyName(p.Name));
-                parameters = baseCtorParameters.Concat(parameters);
+                parameters.AddParameters(constructor.Parameters);
             }
             else if (baseParameters != null)
             {
-                baseCtorParameters = baseParameters.ToArray();
-                baseCtorArgs = baseParameters.Select(p => p.Name).ToArray();
-                parameters = baseCtorParameters.Concat(parameters);
+                parameters.AddBaseParameters(baseParameters);
             }
         }
+
+        parameters.MakeUniqueNames();
 
         var source = new CodeBuilder()
             .AppendHeader()
@@ -132,23 +127,113 @@ public class AutoConstructSourceGenerator : IIncrementalGenerator
         {
             source.AddCompilerGeneratedAttribute().AddGeneratedCodeAttribute();
 
-            if (baseCtorParameters.Any())
+            if (parameters.HasBaseParameters)
             {
-                source.AppendLine($"public {type.Name}({parameters.AsCommaSeparated()}) : base({string.Join(", ", baseCtorArgs)})");
+                source.AppendLine($"public {type.Name}({parameters.ToParameterString()}) : base({parameters.ToBaseParameterString()})");
             }
             else
-                source.AppendLine($"public {type.Name}({parameters.AsCommaSeparated()})");
+            {
+                source.AppendLine($"public {type.Name}({parameters.ToParameterString()})");
+            }
 
             source.StartBlock();
-            foreach (var item in fields)
+            foreach (var f in fields)
             {
-                source.AppendLine($"this.{item.Name} = {CreateFriendlyName(item.Name)};");
+                source.AppendLine($"this.{f.Name} = {parameters.FieldParameterName(f)};");
             }
             source.EndBlock();
         }
 
         return (source, parameters);
     }
+
+    private static bool HasFieldInitialiser(IFieldSymbol symbol)
+    {
+        return symbol.DeclaringSyntaxReferences.Select(x => x.GetSyntax()).OfType<VariableDeclaratorSyntax>().Any(x => x.Initializer != null);
+    }
+}
+
+public class Parameter
+{
+    public Parameter(ITypeSymbol type, string name)
+    {
+        Type = type;
+        Name = name;
+    }
+
+    public ITypeSymbol Type { get; }
+    public string Name { get; set; }
+
+    public override string ToString() =>
+        $"{Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)} {Name}";
+}
+
+public class ParameterList : IEnumerable<Parameter>
+{
+    private readonly Dictionary<IFieldSymbol, Parameter> _fields = new(SymbolEqualityComparer.Default);
+    private readonly List<Parameter> _parameters = new();
+    private readonly Dictionary<Parameter, string> _uniqueNames = new();
+
+    public bool HasBaseParameters => _parameters.Count > 0;
+
+    public ParameterList(IEnumerable<IFieldSymbol> fields)
+    {
+        foreach (var f in fields)
+        {
+            _fields.Add(f, new Parameter(f.Type, CreateFriendlyName(f.Name)));
+        }
+    }
+
+    public void AddParameters(IEnumerable<IParameterSymbol> parameters)
+    {
+        foreach (var p in parameters)
+        {
+            _parameters.Add(new Parameter(p.Type, CreateFriendlyName(p.Name)));
+        }
+    }
+
+    public void AddBaseParameters(IEnumerable<Parameter> baseParameters)
+    {
+        foreach (var p in baseParameters)
+        {
+            _parameters.Add(new Parameter(p.Type, p.Name));
+        }
+    }
+
+    public void MakeUniqueNames()
+    {
+        _uniqueNames.Clear();
+        var nameHash = new HashSet<string>();
+        foreach (var p in this)
+        {
+            var i = 0;
+            var name = p.Name;
+            while (nameHash.Contains(name))
+            {
+                name = p.Name + i++;
+            }
+            nameHash.Add(name);
+            _uniqueNames.Add(p, name);
+        }
+    }
+
+    public string ToParameterString()
+    {
+        return this
+            .Select(p => $"{p.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)} {_uniqueNames[p]}")
+            .AsCommaSeparated();
+    }
+
+    public string ToBaseParameterString()
+    {
+        return _parameters.Select(p => _uniqueNames[p]).AsCommaSeparated();
+    }
+
+    public string FieldParameterName(IFieldSymbol f) => _uniqueNames[_fields[f]];
+    public IEnumerable<Parameter> BaseParameters() => _parameters;
+
+    public IEnumerator<Parameter> GetEnumerator() => _parameters.Concat(_fields.Values).GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     private static string CreateFriendlyName(string name)
     {
@@ -157,25 +242,6 @@ public class AutoConstructSourceGenerator : IIncrementalGenerator
             // Chop off the underscore at the start
             return name.Substring(1);
         }
-
         return name;
-    }
-
-    private static Parameter CreateParameter(IFieldSymbol f) => new(f.Type, CreateFriendlyName(f.Name));
-    private static Parameter CreateParameter(IParameterSymbol p) => new(p.Type, CreateFriendlyName(p.Name));
-
-    private class Parameter
-    {
-        public Parameter(ITypeSymbol type, string name)
-        {
-            Type = type;
-            Name = name;
-        }
-
-        public ITypeSymbol Type { get; set; }
-        public string Name { get; set; }
-
-        public override string ToString() =>
-            $"{Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)} {Name}";
     }
 }
