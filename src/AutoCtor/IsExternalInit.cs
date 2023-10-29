a7b43b41efec1365fb984405af0a799f3fe02ce7
Cam MacFarland <distantcam@gmail.com>
2023-10-29 11:18:35 +0800

    Removed polysharp

---
+++ src/AutoCtor/IsExternalInit.cs
@@ -0,0 +1,5 @@
+﻿namespace System.Runtime.CompilerServices;
+
+[global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+internal static class IsExternalInit { }
