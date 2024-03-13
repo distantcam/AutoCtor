8ba09d3e99a6a875f40a82095957a0c4466c6d53
Simon Cropp <simon.cropp@gmail.com>
2024-03-13 22:12:28 +1100

    Update AutoConstructAttribute.cs

--- src/AutoCtor.Attributes/AutoConstructAttribute.cs
+++ src/AutoCtor.Attributes/AutoConstructAttribute.cs
@@ -20,6 +20,4 @@ public sealed class AutoConstructAttribute : Attribute
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
 [Conditional("AUTOCTOR_USAGES")]
-public sealed class AutoPostConstructAttribute : Attribute
-{
-}
+public sealed class AutoPostConstructAttribute : Attribute;
