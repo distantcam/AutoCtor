4f0449f4c6919606d1a51e8d8a3826a23509c61e
Cam MacFarland <distantcam@gmail.com>
2024-09-05 21:37:38 +0800

    Added derived example

---
+++ src/AutoCtor.Tests/Examples/DerivedExample.cs
@@ -0,0 +1,16 @@
+﻿public interface ILogger<T>
+{
+}
+
+public class Base<TLogger>
+{
+    public Base(ILogger<TLogger> logger)
+    {
+    }
+}
+
+[AutoCtor.AutoConstruct]
+public partial class Derived : Base<Derived>
+{
+    private readonly ILogger<Derived> logger;
+}
