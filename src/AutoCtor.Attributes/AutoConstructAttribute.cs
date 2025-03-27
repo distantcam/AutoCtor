10236c81286669d3bfd9c1553afbfd7fa88bc68c
Cam MacFarland <distantcam@gmail.com>
2025-03-27 21:25:29 +0800

    Removing compile warning

--- src/AutoCtor.Attributes/AutoConstructAttribute.cs
+++ src/AutoCtor.Attributes/AutoConstructAttribute.cs
@@ -12,7 +12,9 @@ public enum GuardSetting
 
 [AttributeUsage(Class | Struct, AllowMultiple = false, Inherited = false)]
 [Conditional("AUTOCTOR_USAGES")]
+#pragma warning disable CS9113 // Parameter is unread.
 public sealed class AutoConstructAttribute(GuardSetting guard = GuardSetting.Default) : Attribute;
+#pragma warning restore CS9113 // Parameter is unread.
 
 [AttributeUsage(Method, AllowMultiple = false, Inherited = false)]
 [Conditional("AUTOCTOR_USAGES")]
