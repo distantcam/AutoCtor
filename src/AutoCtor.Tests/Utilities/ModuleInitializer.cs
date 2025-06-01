592696d19e937b66f84f2d8e1e1c4351785e0573
Cam MacFarland <distantcam@gmail.com>
2025-06-01 21:12:10 +0800

    Added verifier suggested settings

--- src/AutoCtor.Tests/Utilities/ModuleInitializer.cs
+++ src/AutoCtor.Tests/Utilities/ModuleInitializer.cs
@@ -5,6 +5,7 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Init()
     {
+        VerifyDiffPlex.Initialize();
         VerifySourceGenerators.Initialize();
 
         VerifierSettings.ScrubLinesContaining("Version:", "SHA:", "GeneratedCodeAttribute");
---
+++ src/AutoCtor.Tests/VerifyChecksTests.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoCtor.Tests;
+
+public class VerifyChecksTests
+{
+    [Fact]
+    public Task Run() =>
+        VerifyChecks.Run();
+}
