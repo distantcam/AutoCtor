d718b3ac609848c28f7f1a0b68e3fda330f951f5
Cam MacFarland <distantcam@gmail.com>
2025-09-25 19:03:29 +0800

    Testing other scenarios

--- src/AutoCtor.Tests/Examples/PostCtorOptionalInt.cs
+++ src/AutoCtor.Tests/Examples/PostCtorOptionalInt.cs
@@ -1,15 +1,8 @@
-﻿public interface IService { }
-
-public class Service(int value) : IService { public int Value => value; }
-
-[AutoCtor.AutoConstruct]
+﻿[AutoCtor.AutoConstruct]
 public partial class PostCtorOptionalInt
 {
-    private readonly IService _service;
-
     [AutoCtor.AutoPostConstruct]
-    private void Initialize(out IService service, int value = 123)
+    private void Initialize(int value = 123)
     {
-        service = new Service(value);
     }
 }
