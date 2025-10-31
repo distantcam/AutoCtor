14b7cbb9f936ffc0e0a2c631825ace5d17481938
Cam MacFarland <distantcam@gmail.com>
2025-10-31 15:19:47 +0800

    Added to readme

---
+++ src/AutoCtor.Tests/ReadmeExamples/PostConstructWithDefaultParameter.cs
@@ -0,0 +1,16 @@
+﻿using AutoCtor;
+
+public class Service;
+
+#region PostConstructWithDefaultParameter
+
+[AutoConstruct]
+public partial class PostConstructWithDefaultParameter
+{
+    [AutoPostConstruct]
+    private void Initialize(Service? service = default)
+    {
+    }
+}
+
+#endregion
