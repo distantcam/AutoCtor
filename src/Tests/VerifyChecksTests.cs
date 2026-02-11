8e322984d88b25ea70a47b15b99417abe63e7de0
Cam MacFarland <distantcam@users.noreply.github.com>
2026-02-11 12:43:29 +0800

    Removing disabled test

--- src/Tests/VerifyChecksTests.cs
+++
@@ -1,7 +0,0 @@
-﻿internal sealed class VerifyChecksTests
-{
-    [Test]
-    [Skip("These should be more like guidelines instead of rules.")]
-    public Task Run() =>
-        VerifyChecks.Run();
-}
